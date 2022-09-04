d4588003fee07c9e117afc2ce43aaba235124de5
nour-aldin <nourdx73@gmail.com>
2022-09-04 04:37:39 +0200

    Started

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import background from './background.jpg';
+import langIcon from './lang-icon.png';
+import logo from './netflix-logo.png';
+
+export {
+      background,
+      langIcon,
+      logo
+};
